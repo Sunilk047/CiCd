81164888c16c11075c3a63209ba5386ee1171c31
SUNIL KUMAR KUMAWAT <SUNIL.KUMAR.KUMAWAT@GETEPAY.IN>
2024-08-20 12:51:35 +0530

    asdf

---
+++ lib/model.dart
@@ -0,0 +1,10 @@
+// lib/models/widget_model.dart
+
+import 'package:flutter/material.dart';
+
+class WidgetModel {
+  final String id;
+  final Widget widget;
+
+  WidgetModel({required this.id, required this.widget});
+}
